be60d1a78f4d6ecd7281fb71cc7a87c561bc2bf4
MoGood <50001536+MoGood@users.noreply.github.com>
2019-05-18 10:55:44 -0700

    Interface and service

--- src/app/current-wather/current-wather.component.ts
+++ src/app/current-wather/current-wather.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ICurrentWeather } from '../icurrent-weather';
 
 @Component({
   selector: 'app-current-wather',
@@ -6,8 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./current-wather.component.css']
 })
 export class CurrentWatherComponent implements OnInit {
-
-  constructor() { }
+current: ICurrentWeather
+  constructor() {
+this.current = {
+  city: 'Bethesda',
+  country: 'US',
+  date:new Date(),
+  image: '',
+  temperature: 72,
+  description: 'Sunny'
+}
+   }
 
   ngOnInit() {
   }
